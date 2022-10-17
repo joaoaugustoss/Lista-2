e2ba0a398b5581fafe6154597b180f804aad9239
João Augusto dos Santos Silva <joao@MacBook-Air-de-Joao.local>
2022-10-17 20:46:59 -0300

    Adding Documentation

--- src/main.cpp
+++ src/main.cpp
@@ -10,32 +10,6 @@ int main(){
     start = clock();
     graph g;
     list<string> foo;
-    /*foo.push_back("a");
-    foo.push_back("b");
-    foo.push_back("c");
-    foo.push_back("d");
-    foo.push_back("e");
-    foo.push_back("f");
-    foo.push_back("g");
-    foo.push_back("h");
-    foo.push_back("i");
-    g.setVertices(foo);
-    foo.clear();
-    foo.push_back("ab");
-    foo.push_back("ad");
-    foo.push_back("ae");
-    foo.push_back("bc");
-    foo.push_back("bd");
-    foo.push_back("be");
-    foo.push_back("cd");
-    foo.push_back("ce");
-    foo.push_back("cf");
-    foo.push_back("df");
-    foo.push_back("ef");
-    foo.push_back("fg");
-    foo.push_back("gh");
-    foo.push_back("gi");
-    foo.push_back("hi");*/
     foo.push_back("a");
     foo.push_back("b");
     foo.push_back("c");
@@ -67,7 +41,6 @@ int main(){
     for(int i = 0; i < size; i++){
         utilitario::printList(barz[i]);
     }
-
     
     vector<vector<string>> pontes = pn.bruteForce();
     cout << "Todas as pontes encontradas (BF): " << pontes.size() << endl;
@@ -77,27 +50,23 @@ int main(){
             utilitario::printList(*it);
         }
     }
-    ponte vasco = ponte(g);
-    vector<string> fleuryBF = vasco.fleuryBruteF();
+    ponte pbf = ponte(g);
+    vector<string> fleuryBF = pbf.fleuryBruteF();
     cout << "Fleury length: " << fleuryBF.size() << endl;
-    cout << "Fleury path: " << endl;
+    cout << "Eulerian path: ";
     utilitario::printList(fleuryBF);
 
     vector<vector<string>> componentes = ptj.tarjan();
     cout << "Todas as pontes encontradas (Tarjan): " << componentes.size() << endl;
     for(vector<vector<string>>::iterator it = componentes.begin(); it != componentes.end(); it++){
-
         if( !it->empty()){
             utilitario::printList(*it);
         }
     }
     vector<string> fleuryT = ptj.fleuryTarj();
     cout << "Fleury length: " << fleuryT.size() << endl;
-    cout << "Fleury path: " << endl;
+    cout << "Eulerian path: ";
     utilitario::printList(fleuryT);
-
-
-
 
     end = clock();
     double time_taken = double(end - start) / double(CLOCKS_PER_SEC);
